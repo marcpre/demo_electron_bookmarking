13b215a34d3c0900356439d5533c86879578c911
marcpre <marcus.presich@gmail.com>
2019-04-25 06:30:42 +0200

    get bookmark

--- main.js
+++ main.js
@@ -2,16 +2,19 @@
 // Modules
 const {app, ipcMain} = require('electron')
 const mainWindow = require('./mainWindow')
+const readItem = require('./readItem')
 
 // Enable Electron-Reload
 require('electron-reload')(__dirname)
 
 // Listen for new read item
 ipcMain.on('new-item', (e, itemURL) => {
-  console.log(itemURL);
-  setTimeout(() => {
-    e.sender.send('new-item-success', 'new read item')
-  }, 2000)
+
+  // Get read item with readItem module
+  readItem(itemUrl, (item) => {
+    // Send to renderer
+    e.sender.send('new-item-success', item)
+  }) 
 })
 
 // This method will be called when Electron has finished
